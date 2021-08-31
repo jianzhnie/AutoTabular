48ecbfca75fe22018d3e3967e5983c4d5ba28fa0
jianzheng.nie <jianzhnie@126.com>
2021-08-31 18:03:27 +0800

    add tabnet model

---
+++ examples/test_deepnet.py
@@ -0,0 +1,33 @@
+import torch
+import sys
+sys.path.append("../")
+import pandas as pd
+import flash
+from flash.core.data.utils import download_data
+from flash.tabular import TabularClassificationData
+from autotabular.algorithms.deepnets.tabnet import TabularClassifier
+
+
+# 1. load data
+datamodule = TabularClassificationData.from_csv(
+    ["Sex", "Age", "SibSp", "Parch", "Ticket", "Cabin", "Embarked"],
+    "Fare",
+    target_fields="Survived",
+    train_file="../autotabular/datasets/data/Titanic.csv",
+    batch_size=32,
+    val_split=0.1,
+)
+
+# 2. Build the task
+model = TabularClassifier.from_data(datamodule)
+# 3. Create the trainer and train the model
+trainer = flash.Trainer(max_epochs=1, gpus=torch.cuda.device_count())
+trainer.fit(model, datamodule=datamodule)
+
+# 4. Generate predictions from a CSV
+predictions = model.predict("../autotabular/datasets/data/Titanic.csv")
+predictions = pd.core.frame.DataFrame(predictions)
+predictions.rename(columns={0:'Not_suvived',1:'Suvived'}, inplace=True)
+print(predictions)
+# 5. Save the model!
+trainer.save_checkpoint("tabular_classification_model.pt")
