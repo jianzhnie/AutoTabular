e3c04829397e20555c74a57c9978c92610de4c2a
jianzheng.nie <jianzhnie@126.com>
2021-08-31 15:53:18 +0800

    update autotabular

--- autotabular/deepctr/models/lr.py
+++ autotabular/deepctr/models/lr.py
@@ -1,13 +1,11 @@
 import torch
-
-from autotabular.model.layer import FeaturesLinear
+from .layer import FeaturesLinear
 
 
 class LogisticRegressionModel(torch.nn.Module):
     """
     A pytorch implementation of Logistic Regression.
     """
-
     def __init__(self, field_dims):
         super().__init__()
         self.linear = FeaturesLinear(field_dims)
