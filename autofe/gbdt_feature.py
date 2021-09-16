c31511ea690335640f4869fae81a9e79b83e9fdf
jianzhnie <jianzhnie@126.com>
2021-09-16 22:00:09 +0800

    add gbdt features

--- autofe/gbdt_feature.py
+++ autofe/gbdt_feature.py
@@ -1,9 +1,125 @@
 import numpy as np
 import pandas as pd
+from catboost import CatBoostClassifier, CatBoostRegressor
+from lightgbm.sklearn import LGBMClassifier, LGBMRegressor
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor, RandomForestClassifier, RandomForestRegressor
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from xgboost.sklearn import XGBClassifier, XGBRegressor
+
+
+class BaseGbdtEmbedding(BaseEstimator):
+
+    def __init__(self, base_estimatior=None):
+        self.base_estimator = base_estimatior
+
+    def fit(self, X, y=None, sample_weight=None):
+        """Fit estimator.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            The input samples. Use ``dtype=np.float32`` for maximum
+            efficiency. Sparse matrices are also supported, use sparse
+            ``csc_matrix`` for maximum efficiency.
+
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        sample_weight : array-like of shape (n_samples,), default=None
+            Sample weights. If None, then samples are equally weighted. Splits
+            that would create child nodes with net zero or negative weight are
+            ignored while searching for a split in each node. In the case of
+            classification, splits are also ignored if they would result in any
+            single class carrying a negative weight in either child node.
+
+        Returns
+        -------
+        self : object
+        """
+        self.fit_transform(X, y, sample_weight=sample_weight)
+        return self
+
+    def fit_transform(self, X, y=None, sample_weight=None):
+        """Fit estimator and transform dataset.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Input data used to build forests. Use ``dtype=np.float32`` for
+            maximum efficiency.
+
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        sample_weight : array-like of shape (n_samples,), default=None
+            Sample weights. If None, then samples are equally weighted. Splits
+            that would create child nodes with net zero or negative weight are
+            ignored while searching for a split in each node. In the case of
+            classification, splits are also ignored if they would result in any
+            single class carrying a negative weight in either child node.
+
+        Returns
+        -------
+        X_transformed : sparse matrix of shape (n_samples, n_out)
+            Transformed dataset.
+        """
+        self.base_estimator.fit(X, y, sample_weight=sample_weight)
+        self.one_hot_encoder_ = OneHotEncoder(sparse=self.sparse_output)
+        return self.one_hot_encoder_.fit_transform(self.apply(X))
+
+    def transform(self, X):
+        """Transform dataset.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Input data to be transformed. Use ``dtype=np.float32`` for maximum
+            efficiency. Sparse matrices are also supported, use sparse
+            ``csr_matrix`` for maximum efficiency.
+
+        Returns
+        -------
+        X_transformed : sparse matrix of shape (n_samples, n_out)
+            Transformed dataset.
+        """
+        return self.one_hot_encoder_.transform(self.apply(X))
+
+
+def get_estimator_class(task, estimator_name):
+    """when adding a new learner, need to add an elif branch."""
+
+    if 'xgboost' == estimator_name:
+        if 'regression' == task:
+            estimator_class = XGBRegressor
+        else:
+            estimator_class = XGBClassifier
+    elif 'rf' == estimator_name:
+        if 'regression' == task:
+            estimator_class = RandomForestRegressor
+        else:
+            estimator_class = RandomForestClassifier
+    elif 'gbdt' == estimator_name:
+        if 'regression' == task:
+            estimator_class = GradientBoostingClassifier
+        else:
+            estimator_class = GradientBoostingRegressor
+    elif 'lgbm' == estimator_name:
+        if 'regression' == task:
+            estimator_class = LGBMRegressor
+        else:
+            estimator_class = LGBMClassifier
+    elif 'catboost' == estimator_name:
+        if 'regression' == task:
+            estimator_class = CatBoostClassifier
+        else:
+            estimator_class = CatBoostRegressor
+    else:
+        raise ValueError(
+            estimator_name + ' is not a built-in learner. '
+            'Please use AutoML.add_learner() to add a customized learner.')
+    return estimator_class
 
 
 class GBDTFeaturesCountVecTransformer(BaseEstimator, ClassifierMixin):
@@ -34,6 +150,31 @@ class GBDTFeaturesCountVecTransformer(BaseEstimator, ClassifierMixin):
         if self.vectorizer is not None:
             leaf = [' '.join(item) for item in leaf]
             result = self.vectorizer.transform(leaf)
+        return result
+
+
+class XGBoostFeatureTransformer(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, task, concate_fea=False):
+
+        self.task = task
+        self.concate_fea = concate_fea
+
+        self.xgb = XGBClassifier(
+            n_estimators=100, learning_rate=1.0, max_depth=3, random_state=0)
+        self.onehot = OneHotEncoder()
+
+    def fit(self, X, y):
+        self.gbdt.fit(X, y)
+        return self
+
+    def predict_proba(self, X):
+        gbdt_leaf = self.gbdt.apply(X)[:, :, 0]
+        gbdt_feats_name = [
+            'gbdt_leaf_' + str(i) for i in range(gbdt_leaf.shape[1])
+        ]
+        gbdt_feats = pd.DataFrame(gbdt_leaf, columns=gbdt_feats_name)
+        result = pd.concat([X, gbdt_feats], axis=1)
         return result
 
 
