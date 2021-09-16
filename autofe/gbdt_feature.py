2979ce36d7e4b1bdf84fcc391f4ab5c2f9934f54
Robin <jianzhnie@126.com>
2021-09-16 16:00:22 +0800

    update gbdt_feature

--- autofe/gbdt_feature.py
+++ autofe/gbdt_feature.py
@@ -1,33 +1,12 @@
+import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.feature_extraction.text import CountVectorizer
-
-params = {
-    'task': 'train',
-    'boosting_type': 'gbdt',
-    'objective': 'binary',
-    'metric': {'binary_logloss'},
-    'num_leaves': 64,
-    'num_trees': 100,
-    'learning_rate': 0.01,
-    'feature_fraction': 0.9,
-    'bagging_fraction': 0.8,
-    'bagging_freq': 5,
-    'verbose': 0
-}
-
-gbm_reg_params = dict(
-    objective='binary',
-    subsample=0.8,
-    min_child_weight=0.5,
-    colsample_bytree=0.7,
-    num_leaves=100,
-    max_depth=12,
-    learning_rate=0.05,
-    n_estimators=10,
-)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
-class GBDTFeatures(BaseEstimator, ClassifierMixin):
+class GBDTFeaturesCountVecTransformer(BaseEstimator, ClassifierMixin):
 
     def __init__(self,
                  gbdt=None,
@@ -58,11 +37,103 @@ class GBDTFeatures(BaseEstimator, ClassifierMixin):
         return result
 
 
+class GBDTFeatureTransformer(BaseEstimator, ClassifierMixin):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.gbdt = GradientBoostingClassifier(
+            n_estimators=100, learning_rate=1.0, max_depth=3, random_state=0)
+        self.onehot = OneHotEncoder()
+
+    def fit(self, X, y):
+        self.gbdt.fit(X, y)
+        return self
+
+    def predict_proba(self, X):
+        gbdt_leaf = self.gbdt.apply(X)[:, :, 0]
+        gbdt_feats_name = [
+            'gbdt_leaf_' + str(i) for i in range(gbdt_leaf.shape[1])
+        ]
+        gbdt_feats = pd.DataFrame(gbdt_leaf, columns=gbdt_feats_name)
+        result = pd.concat([X, gbdt_feats], axis=1)
+        return result
+
+
+class LightGBMFeatureTransformer(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, gbdt=None, gbdt_params=None):
+        self.gbdt = gbdt(**gbdt_params)
+
+    def fit(self, X, y):
+        self.gbdt.fit(X, y)
+        return self
+
+    def predict_proba(self, X):
+        gbdt_leaf = self.gbdt.predict(X, pred_leaf=True)
+        gbdt_feats_name = [
+            'gbdt_leaf_' + str(i) for i in range(gbdt_leaf.shape[1])
+        ]
+        gbdt_feats = pd.DataFrame(gbdt_leaf, columns=gbdt_feats_name)
+        result = pd.concat([X, gbdt_feats], axis=1)
+        return result
+
+
 if __name__ == '__main__':
-    from sklearn.datasets import load_iris
     import lightgbm as lgb
-    X_train, y_train = load_iris(return_X_y=True)
-    clf = GBDTFeatures(
-        gbdt=lgb.LGBMClassifier, gbdt_params={'n_estimators': 500})
+    titanic = pd.read_csv('autotabular/datasets/data/Titanic.csv')
+    # 'Embarked' is stored as letters, so fit a label encoder to the train set to use in the loop
+    embarked_encoder = LabelEncoder()
+    embarked_encoder.fit(titanic['Embarked'].fillna('Null'))
+    # Record anyone travelling alone
+    titanic['Alone'] = (titanic['SibSp'] == 0) & (titanic['Parch'] == 0)
+    # Transform 'Embarked'
+    titanic['Embarked'].fillna('Null', inplace=True)
+    titanic['Embarked'] = embarked_encoder.transform(titanic['Embarked'])
+    # Transform 'Sex'
+    titanic.loc[titanic['Sex'] == 'female', 'Sex'] = 0
+    titanic.loc[titanic['Sex'] == 'male', 'Sex'] = 1
+    titanic['Sex'] = titanic['Sex'].astype('int8')
+    # Drop features that seem unusable. Save passenger ids if test
+    titanic.drop(['Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+
+    trainMeans = titanic.groupby(['Pclass', 'Sex'])['Age'].mean()
+
+    def f(x):
+        if not np.isnan(x['Age']):  # not NaN
+            return x['Age']
+        return trainMeans[x['Pclass'], x['Sex']]
+
+    titanic['Age'] = titanic.apply(f, axis=1)
+    X_train = titanic.drop(['Survived'], axis=1)
+    y_train = titanic['Survived']
+
+    params = {
+        'boosting': 'gbdt',
+        'objective': 'binary',
+        'metric': 'binary_logloss',
+        'num_leaves': 64,
+        'num_trees': 100,
+        'learning_rate': 0.01,
+        'feature_fraction': 0.9,
+        'bagging_fraction': 0.8,
+        'bagging_freq': 5,
+        'min_data_in_leaf': 4,
+        'verbose': 0
+    }
+    clf = GBDTFeaturesCountVecTransformer(
+        gbdt=lgb.LGBMClassifier, gbdt_params=params)
     clf.fit(X_train, y_train)
     result = clf.predict_proba(X_train)
+    print(result)
+
+    clf = LightGBMFeatureTransformer(
+        gbdt=lgb.LGBMClassifier, gbdt_params=params)
+    clf.fit(X_train, y_train)
+    result = clf.predict_proba(X_train)
+    print(result)
+
+    clf = GBDTFeatureTransformer()
+    clf.fit(X_train, y_train)
+    result = clf.predict_proba(X_train)
+    print(result)
