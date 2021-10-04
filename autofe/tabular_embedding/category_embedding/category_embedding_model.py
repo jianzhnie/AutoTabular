ed1298a18463dc3b73c58c62d27863e2c1c479bf
jianzheng.nie <jianzhnie@126.com>
2021-10-04 18:49:37 +0800

    add tabular embedding

---
+++ autofe/tabular_embedding/category_embedding/category_embedding_model.py
@@ -0,0 +1,193 @@
+# Pytorch Tabular
+# Author: Manu Joseph <manujoseph@gmail.com>
+# For license information, see LICENSE.TXT
+"""Category Embedding Model."""
+import logging
+from dataclasses import dataclass, field
+from typing import Dict
+
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from omegaconf import DictConfig
+from pytorch_tabular.config import ModelConfig
+from pytorch_tabular.models import BaseModel
+from pytorch_tabular.utils import _initialize_layers, _linear_dropout_bn
+
+logger = logging.getLogger(__name__)
+
+
+class FeedForwardBackbone(pl.LightningModule):
+
+    def __init__(self, config: DictConfig, **kwargs):
+        self.embedding_cat_dim = sum([y for x, y in config.embedding_dims])
+        super().__init__()
+        self.save_hyperparameters(config)
+        self._build_network()
+
+    def _build_network(self):
+        # Linear Layers
+        layers = []
+        _curr_units = self.embedding_cat_dim + self.hparams.continuous_dim
+        if self.hparams.embedding_dropout != 0 and self.embedding_cat_dim != 0:
+            layers.append(nn.Dropout(self.hparams.embedding_dropout))
+        for units in self.hparams.layers.split('-'):
+            layers.extend(
+                _linear_dropout_bn(
+                    self.hparams.activation,
+                    self.hparams.initialization,
+                    self.hparams.use_batch_norm,
+                    _curr_units,
+                    int(units),
+                    self.hparams.dropout,
+                ))
+            _curr_units = int(units)
+        self.linear_layers = nn.Sequential(*layers)
+        self.output_dim = _curr_units
+
+    def forward(self, x):
+        x = self.linear_layers(x)
+        return x
+
+
+class CategoryEmbeddingModel(BaseModel):
+
+    def __init__(self, config: DictConfig, **kwargs):
+        # The concatenated output dim of the embedding layer
+        self.embedding_cat_dim = sum([y for x, y in config.embedding_dims])
+        super().__init__(config, **kwargs)
+
+    def _build_network(self):
+        # Embedding layers
+        self.embedding_layers = nn.ModuleList(
+            [nn.Embedding(x, y) for x, y in self.hparams.embedding_dims])
+        # Continuous Layers
+        if self.hparams.batch_norm_continuous_input:
+            self.normalizing_batch_norm = nn.BatchNorm1d(
+                self.hparams.continuous_dim)
+        # Backbone
+        self.backbone = FeedForwardBackbone(self.hparams)
+        # Adding the last layer
+        self.output_layer = nn.Linear(
+            self.backbone.output_dim, self.hparams.output_dim
+        )  # output_dim auto-calculated from other config
+        _initialize_layers(self.hparams.activation,
+                           self.hparams.initialization, self.output_layer)
+
+    def unpack_input(self, x: Dict):
+        continuous_data, categorical_data = x['continuous'], x['categorical']
+        if self.embedding_cat_dim != 0:
+            x = []
+            # for i, embedding_layer in enumerate(self.embedding_layers):
+            #     x.append(embedding_layer(categorical_data[:, i]))
+            x = [
+                embedding_layer(categorical_data[:, i])
+                for i, embedding_layer in enumerate(self.embedding_layers)
+            ]
+            x = torch.cat(x, 1)
+
+        if self.hparams.continuous_dim != 0:
+            if self.hparams.batch_norm_continuous_input:
+                continuous_data = self.normalizing_batch_norm(continuous_data)
+
+            if self.embedding_cat_dim != 0:
+                x = torch.cat([x, continuous_data], 1)
+            else:
+                x = continuous_data
+        return x
+
+    def forward(self, x: Dict):
+        x = self.unpack_input(x)
+        x = self.backbone(x)
+        y_hat = self.output_layer(x)
+        if (self.hparams.task == 'regression') and (self.hparams.target_range
+                                                    is not None):
+            for i in range(self.hparams.output_dim):
+                y_min, y_max = self.hparams.target_range[i]
+                y_hat[:, i] = y_min + nn.Sigmoid()(y_hat[:, i]) * (
+                    y_max - y_min)
+        return {'logits': y_hat, 'backbone_features': x}
+
+
+@dataclass
+class CategoryEmbeddingModelConfig(ModelConfig):
+    """CategoryEmbeddingModel configuration
+    Args:
+        task (str): Specify whether the problem is regression of classification.Choices are: regression classification
+        learning_rate (float): The learning rate of the model
+        loss (Union[str, NoneType]): The loss function to be applied.
+            By Default it is MSELoss for regression and CrossEntropyLoss for classification.
+            Unless you are sure what you are doing, leave it at MSELoss or L1Loss for regression and CrossEntropyLoss for classification
+        metrics (Union[List[str], NoneType]): the list of metrics you need to track during training.
+            The metrics should be one of the metrics implemented in PyTorch Lightning.
+            By default, it is Accuracy if classification and MeanSquaredLogError for regression
+        metrics_params (Union[List, NoneType]): The parameters to be passed to the Metrics initialized
+        target_range (Union[List, NoneType]): The range in which we should limit the output variable. Currently ignored for multi-target regression
+            Typically used for Regression problems. If left empty, will not apply any restrictions
+
+        layers (str): Hyphen-separated number of layers and units in the classification head. eg. 32-64-32.
+        batch_norm_continuous_input (bool): If True, we will normalize the contiinuous layer by passing it through a BatchNorm layer
+        activation (str): The activation type in the classification head.
+            The default activation in PyTorch like ReLU, TanH, LeakyReLU, etc.
+            https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity
+        embedding_dims (Union[List[int], NoneType]): The dimensions of the embedding for each categorical column
+            as a list of tuples (cardinality, embedding_dim). If left empty, will infer using the cardinality of the categorical column
+            using the rule min(50, (x + 1) // 2)
+        embedding_dropout (float): probability of an embedding element to be zeroed.
+        dropout (float): probability of an classification element to be zeroed.
+        use_batch_norm (bool): Flag to include a BatchNorm layer after each Linear Layer+DropOut
+        initialization (str): Initialization scheme for the linear layers. Choices are: `kaiming` `xavier` `random`
+
+    Raises:
+        NotImplementedError: Raises an error if task is not in ['regression','classification']
+    """
+
+    layers: str = field(
+        default='128-64-32',
+        metadata={
+            'help':
+            'Hyphen-separated number of layers and units in the classification head. eg. 32-64-32.'
+        },
+    )
+    batch_norm_continuous_input: bool = field(
+        default=True,
+        metadata={
+            'help':
+            'If True, we will normalize the contiinuous layer by passing it through a BatchNorm layer'
+        },
+    )
+    activation: str = field(
+        default='ReLU',
+        metadata={
+            'help':
+            'The activation type in the classification head. The default activaion in PyTorch like ReLU, TanH, \
+                LeakyReLU, etc. https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity'
+        },
+    )
+    embedding_dropout: float = field(
+        default=0.5,
+        metadata={'help': 'probability of an embedding element to be zeroed.'},
+    )
+    dropout: float = field(
+        default=0.5,
+        metadata={
+            'help': 'probability of an classification element to be zeroed.'
+        },
+    )
+    use_batch_norm: bool = field(
+        default=False,
+        metadata={
+            'help':
+            'Flag to include a BatchNorm layer after each Linear Layer+DropOut'
+        },
+    )
+    initialization: str = field(
+        default='kaiming',
+        metadata={
+            'help': 'Initialization scheme for the linear layers',
+            'choices': ['kaiming', 'xavier', 'random'],
+        },
+    )
+    _module_src: str = field(default='category_embedding')
+    _model_name: str = field(default='CategoryEmbeddingModel')
+    _config_name: str = field(default='CategoryEmbeddingModelConfig')
---
+++ autofe/tabular_embedding/tabulardatatransformer.py
@@ -0,0 +1,180 @@
+from pathlib import Path
+
+import pandas as pd
+import wget
+from pytorch_tabular.config import DataConfig, OptimizerConfig, TrainerConfig
+from pytorch_tabular.feature_extractor import DeepFeatureExtractor
+from pytorch_tabular.models.category_embedding.config import CategoryEmbeddingModelConfig
+from pytorch_tabular.tabular_model import TabularModel
+from sklearn.model_selection import train_test_split
+
+
+class TabularEmbeddingTransformer():
+
+    def __init__(self,
+                 cat_col_names=None,
+                 num_col_names=None,
+                 target_name=None,
+                 num_classes=None):
+
+        data_config = DataConfig(
+            target=target_name,
+            continuous_cols=num_col_names,
+            categorical_cols=cat_col_names,
+            continuous_feature_transform=None,
+            normalize_continuous_features=False,
+        )
+        model_config = CategoryEmbeddingModelConfig(
+            task='classification',
+            metrics=['f1', 'accuracy'],
+            metrics_params=[{
+                'num_classes': num_classes
+            }, {}])
+
+        trainer_config = TrainerConfig(
+            gpus=-1,
+            auto_select_gpus=True,
+            fast_dev_run=True,
+            max_epochs=120,
+            batch_size=128)
+
+        optimizer_config = OptimizerConfig()
+
+        self.tabular_model = TabularModel(
+            data_config=data_config,
+            model_config=model_config,
+            optimizer_config=optimizer_config,
+            trainer_config=trainer_config,
+        )
+
+    def fit(self, train_data):
+        """Just for compatibility.
+
+        Does not do anything
+        """
+        self.tabular_model.fit(train=train_data)
+        self.transformerMoldel = DeepFeatureExtractor(
+            self.tabular_model, drop_original=False)
+        return self
+
+    def transform(self, train_data):
+        X_encoded = self.transformerMoldel.transform(train_data)
+        return X_encoded
+
+    def fit_transform(self, train_data):
+        """Encode given columns of X based on the learned features.
+
+        Args:
+            X (pd.DataFrame): DataFrame of features, shape (n_samples, n_features). Must contain columns to encode.
+            y ([type], optional): Only for compatibility. Not used. Defaults to None.
+
+        Returns:
+            pd.DataFrame: The encoded dataframe
+        """
+        self.fit(train_data)
+        return self.transform(train_data)
+
+
+if __name__ == '__main__':
+    BASE_DIR = Path.home().joinpath('data')
+    datafile = BASE_DIR.joinpath('covtype.data.gz')
+    datafile.parent.mkdir(parents=True, exist_ok=True)
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz'
+    if not datafile.exists():
+        wget.download(url, datafile.as_posix())
+    target_name = ['Covertype']
+    cat_col_names = [
+        'Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3',
+        'Wilderness_Area4', 'Soil_Type1', 'Soil_Type2', 'Soil_Type3',
+        'Soil_Type4', 'Soil_Type5', 'Soil_Type6', 'Soil_Type7', 'Soil_Type8',
+        'Soil_Type9', 'Soil_Type10', 'Soil_Type11', 'Soil_Type12',
+        'Soil_Type13', 'Soil_Type14', 'Soil_Type15', 'Soil_Type16',
+        'Soil_Type17', 'Soil_Type18', 'Soil_Type19', 'Soil_Type20',
+        'Soil_Type21', 'Soil_Type22', 'Soil_Type23', 'Soil_Type24',
+        'Soil_Type25', 'Soil_Type26', 'Soil_Type27', 'Soil_Type28',
+        'Soil_Type29', 'Soil_Type30', 'Soil_Type31', 'Soil_Type32',
+        'Soil_Type33', 'Soil_Type34', 'Soil_Type35', 'Soil_Type36',
+        'Soil_Type37', 'Soil_Type38', 'Soil_Type39', 'Soil_Type40'
+    ]
+
+    num_col_names = [
+        'Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology',
+        'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways',
+        'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm',
+        'Horizontal_Distance_To_Fire_Points'
+    ]
+
+    feature_columns = (num_col_names + cat_col_names + target_name)
+
+    df = pd.read_csv(datafile, header=None, names=feature_columns)
+
+    train, test = train_test_split(df, random_state=42)
+    train, val = train_test_split(train, random_state=42)
+    num_classes = len(set(train[target_name].values.ravel()))
+
+    # data_config = DataConfig(
+    #     target=target_name,
+    #     continuous_cols=num_col_names,
+    #     categorical_cols=cat_col_names,
+    #     continuous_feature_transform=None,  #"quantile_normal",
+    #     normalize_continuous_features=False,
+    # )
+    # model_config = CategoryEmbeddingModelConfig(
+    #     task='classification',
+    #     metrics=['f1', 'accuracy'],
+    #     metrics_params=[{
+    #         'num_classes': num_classes
+    #     }, {}])
+    # model_config = NodeConfig(
+    #     task='classification',
+    #     depth=4,
+    #     num_trees=1024,
+    #     input_dropout=0.0,
+    #     metrics=['f1', 'accuracy'],
+    #     metrics_params=[{
+    #         'num_classes': num_classes,
+    #         'average': 'macro'
+    #     }, {}],
+    # )
+    # model_config = TabTransformerConfig(
+    #     task='classification',
+    #     metrics=['f1', 'accuracy'],
+    #     share_embedding=True,
+    #     share_embedding_strategy='add',
+    #     shared_embedding_fraction=0.25,
+    #     metrics_params=[{
+    #         'num_classes': num_classes,
+    #         'average': 'macro'
+    #     }, {}],
+    # )
+
+    # trainer_config = TrainerConfig(
+    #     gpus=-1,
+    #     auto_select_gpus=True,
+    #     fast_dev_run=True,
+    #     max_epochs=5,
+    #     batch_size=512)
+    # experiment_config = ExperimentConfig(
+    #     project_name='PyTorch Tabular Example',
+    #     run_name='node_forest_cov',
+    #     exp_watch='gradients',
+    #     log_target='wandb',
+    #     log_logits=True)
+    # optimizer_config = OptimizerConfig()
+    # tabular_model = TabularModel(
+    #     data_config=data_config,
+    #     model_config=model_config,
+    #     optimizer_config=optimizer_config,
+    #     trainer_config=trainer_config,
+    #     experiment_config=experiment_config,
+    # )
+    # sampler = get_balanced_sampler(train[target_name].values.ravel())
+    # tabular_model.fit(train=train, validation=val, train_sampler=sampler)
+
+    transformer = TabularEmbeddingTransformer(
+        cat_col_names=cat_col_names,
+        num_col_names=num_col_names,
+        target_name=target_name,
+        num_classes=num_classes)
+    train_transform = transformer.fit_transform(df)
+    print(train_transform)
