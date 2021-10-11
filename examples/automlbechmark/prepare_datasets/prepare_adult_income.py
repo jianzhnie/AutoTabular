aad5e8fa84e987c5647a6c6e0b20490f2178d41f
jianzheng.nie <jianzhnie@126.com>
2021-10-11 15:33:43 +0800

    add prepare datasets

--- examples/automlbechmark/prepare_datasets/prepare_adult_income.py
+++ examples/automlbechmark/prepare_datasets/prepare_adult_income.py
@@ -3,13 +3,11 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 
-pd.options.display.max_columns = 100
-
 SEED = 1
-
-ROOT_DIR = Path(os.getcwd())
+ROOT_DIR = Path('/home/robin/jianzh/autotabular/examples/automlbechmark')
 RAW_DATA_DIR = ROOT_DIR / 'data/raw_data/adult'
 PROCESSED_DATA_DIR = ROOT_DIR / 'data/processed_data/adult'
 if not os.path.isdir(PROCESSED_DATA_DIR):
@@ -38,8 +36,18 @@ adult_train = pd.read_csv(RAW_DATA_DIR / 'adult.data', names=colnames)
 adult_test = pd.read_csv(
     RAW_DATA_DIR / 'adult.test', skiprows=1, names=colnames)
 adult = pd.concat([adult_train, adult_test])
-adult = adult.replace(to_replace=' ?', value=np.nan).dropna()
-adult = adult.sample(frac=1).reset_index(drop=True)
+
+# fill na
+adult = adult.replace(to_replace=' ?', value=np.nan)
+fill_transformer = SimpleImputer(
+    missing_values=np.nan, strategy='most_frequent')
+adult = fill_transformer.fit_transform(adult)
+adult = pd.DataFrame(adult, columns=colnames)
+
+# types convert
+adult.age = adult.age.astype(float)
+adult['hours_per_week'] = adult['hours_per_week'].astype(float)
+
 for c in adult.columns:
     try:
         adult[c] = adult[c].str.lower()
@@ -48,13 +56,19 @@ for c in adult.columns:
 
 adult['target'] = (adult['income'].apply(lambda x: '>50' in x)).astype(int)
 adult.drop('income', axis=1, inplace=True)
+adult.drop('education_num', axis=1, inplace=True)
+adult = adult.sample(frac=1).reset_index(drop=True)
 
+print(adult)
 print(adult.head())
+print(adult.describe(include=['O']))
+print(adult.info())
+
 adult_train, adult_test = train_test_split(
     adult, test_size=0.2, random_state=SEED, stratify=adult.target)
 adult_val, adult_test = train_test_split(
     adult_test, test_size=0.5, random_state=SEED, stratify=adult_test.target)
 
-adult_train.to_pickle(PROCESSED_DATA_DIR / 'adult_train.pkl')
-adult_val.to_pickle(PROCESSED_DATA_DIR / 'adult_val.pkl')
-adult_test.to_pickle(PROCESSED_DATA_DIR / 'adult_test.pkl')
+adult_train.to_csv(PROCESSED_DATA_DIR / 'adult_train.csv', index=None)
+adult_val.to_csv(PROCESSED_DATA_DIR / 'adult_val.csv', index=None)
+adult_test.to_csv(PROCESSED_DATA_DIR / 'adult_test.csv', index=None)
