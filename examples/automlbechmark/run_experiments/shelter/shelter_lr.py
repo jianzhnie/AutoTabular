86aebda6b568f0ca67dd3826d3f289945271329c
jianzheng.nie <jianzhnie@126.com>
2021-10-20 13:42:48 +0800

    add shelter data

--- examples/automlbechmark/run_experiments/shelter/shelter_lr.py
+++ examples/automlbechmark/run_experiments/shelter/shelter_lr.py
@@ -1,29 +1,33 @@
-import numpy as np
+from pathlib import Path
+
 import pandas as pd
-from autofe.get_feature import *
-from autofe.deeptabular_utils import LabelEncoder
+from autofe.get_feature import get_baseline_total_data, train_and_evaluate
 from sklearn.linear_model import LogisticRegression
 
-
 if __name__ == '__main__':
-    root_path = './data/shelter/'
-    train_data = pd.read_csv(root_path + 'data_train.csv')
-    len_train = len(train_data)
-    test_data = pd.read_csv(root_path + 'data_test.csv')
+    ROOTDIR = Path('./')
+    PROCESSED_DATA_DIR = ROOTDIR / 'data/processed_data/shelter/'
+
+    train_datafile = PROCESSED_DATA_DIR / 'train_data.csv'
+    test_datafile = PROCESSED_DATA_DIR / 'test_data.csv'
+
+    train_data = pd.read_csv(PROCESSED_DATA_DIR / 'train_data.csv')
+    test_data = pd.read_csv(PROCESSED_DATA_DIR / 'test_data.csv')
     total_data = pd.concat([train_data, test_data]).reset_index(drop=True)
+    len_train = len(train_data)
 
     target_name = 'OutcomeType'
-
-    classfier = LogisticRegression(multi_class="multinomial",random_state=0)
-    
-
-
+    classfier = LogisticRegression(multi_class='multinomial', random_state=0)
     """lr baseline"""
     # Accuracy: 0.5791245791245792.
     total_data_base = get_baseline_total_data(total_data)
     # cat_col_names = get_category_columns(total_data, target_name)
     # label_encoder = LabelEncoder(cat_col_names)
     # total_data_base = label_encoder.fit_transform(total_data)
-    print("lr baseline: ")
-    acc = train_and_evaluate(total_data_base, target_name, len_train,
-                                  classfier, task_type='multiclass')
+    print('lr baseline: ')
+    acc = train_and_evaluate(
+        total_data_base,
+        target_name,
+        len_train,
+        classfier,
+        task_type='multiclass')
--- examples/automlbechmark/run_experiments/shelter/shelter_xgboost.py
+++ examples/automlbechmark/run_experiments/shelter/shelter_xgboost.py
@@ -1,33 +1,33 @@
+from pathlib import Path
 
-import numpy as np
 import pandas as pd
-from autofe.get_feature import *
 from autofe.deeptabular_utils import LabelEncoder
-from xgboost import XGBClassifier 
+from autofe.get_feature import get_baseline_total_data, get_category_columns, train_and_evaluate
+from xgboost import XGBClassifier
 
 if __name__ == '__main__':
-    
-    root_path = './data/shelter/'
-    train_data = pd.read_csv(root_path + 'data_train.csv')
-    len_train = len(train_data)
-    test_data = pd.read_csv(root_path + 'data_test.csv')
+    ROOTDIR = Path('./')
+    PROCESSED_DATA_DIR = ROOTDIR / 'data/processed_data/shelter/'
+
+    train_datafile = PROCESSED_DATA_DIR / 'train_data.csv'
+    test_datafile = PROCESSED_DATA_DIR / 'test_data.csv'
+
+    train_data = pd.read_csv(PROCESSED_DATA_DIR / 'train_data.csv')
+    test_data = pd.read_csv(PROCESSED_DATA_DIR / 'test_data.csv')
     total_data = pd.concat([train_data, test_data]).reset_index(drop=True)
+    len_train = len(train_data)
 
     target_name = 'OutcomeType'
-
     classfier = XGBClassifier(objective='multi:softprob')
-    # estimator = LGBMClassifier(objective='binary')
-
-    """xgboost baseline"""
-    # Accuracy: 0.8724279835390947. ROC_AUC: 0.9257845633487581
-    # total_data_base = get_baseline_total_data(total_data)
+    """XGBClassifier baseline"""
+    total_data_base = get_baseline_total_data(total_data)
     cat_col_names = get_category_columns(total_data, target_name)
     label_encoder = LabelEncoder(cat_col_names)
     total_data_base = label_encoder.fit_transform(total_data)
-    print("xgboost baseline: \n")
-    acc = train_and_evaluate(total_data_base, target_name, len_train,
-                                  classfier, task_type='multiclass')
-
-
-
-    
+    print('XGBClassifier baseline: ')
+    acc = train_and_evaluate(
+        total_data_base,
+        target_name,
+        len_train,
+        classfier,
+        task_type='multiclass')
