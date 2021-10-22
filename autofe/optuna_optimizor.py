035fd1b793b657acb3f138d94ba5975021a148ff
jianzhnie <jianzhnie@126.com>
2021-10-22 09:03:02 +0800

    Update optuna_optimizor.py

--- autofe/optuna_optimizor.py
+++ autofe/optuna_optimizor.py
@@ -1,22 +1,30 @@
+import logging
 from typing import Dict
 
+import numpy as np
 import optuna
+import pandas as pd
 import sklearn.datasets
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.metrics import accuracy_score, r2_score, roc_auc_score
+from sklearn.metrics import accuracy_score, mean_squared_error, r2_score, roc_auc_score
 from sklearn.model_selection import train_test_split
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class RandomForestHpo(object):
 
     def __init__(self,
+                 target: str = None,
                  task: str = 'binary',
                  metric: str = 'accuracy',
                  verbose: bool = False):
 
+        self.taget = target
         self.task = task
         self.metric = metric
-        self.verbose = verbose
         self.early_stop_dict: Dict = {}
 
         if self.task == 'regression':
@@ -32,14 +40,15 @@ class RandomForestHpo(object):
             max_evals: int = 10,
             timeout=600):
         objective = self.get_objective(X_train, y_train, X_val, y_val)
+        logger.info('Beginning  RandomForest Hpo training ...')
         study = optuna.create_study(direction='maximize')
         study.optimize(objective, n_trials=max_evals, timeout=timeout)
         trial = study.best_trial
         best_param = trial.params
-        print()
-        print('Finished  HPO training, get the best model params: ')
-        print(best_param)
-        print('Retraining on the whole dataset')
+        logging.info('Finished RandomForest Hpo trainingc ...')
+        logging.info('Get the best model params ...')
+        logging.info(best_param)
+        logging.info('Retraining on the whole dataset')
         self.model = self.estimator(**best_param).fit(X_train, y_train)
         return best_param
 
@@ -51,13 +60,23 @@ class RandomForestHpo(object):
         preds = self.model.predict_proba(X_test)
         return preds
 
-    def get_score_fn(self, task):
-        score_dict = {
+    def get_score_fn(self, task, metric):
+        support_metric_dict = {
+            'auc': roc_auc_score,
+            'accuracy': accuracy_score,
+            'r2': r2_score,
+            'mse': mean_squared_error
+        }
+        default_score_dict = {
             'binary': roc_auc_score,
             'multiclass': accuracy_score,
             'regression': r2_score,
         }
-        return score_dict[task]
+        if metric is not None:
+            score_fn = support_metric_dict[metric]
+        else:
+            score_fn = default_score_dict[task]
+        return score_fn
 
     def get_objective(self,
                       X_train,
@@ -91,18 +110,46 @@ class RandomForestHpo(object):
 
             model = estimator.fit(X_train, y_train)
             preds = model.predict(X_val)
-            score_fn = self.get_score_fn(self.task)
+            score_fn = self.get_score_fn(self.task, self.metric)
             score = score_fn(y_val, preds)
 
             return score
 
         return objective
 
+    def _validate_fit_data(self, train_data, tuning_data=None):
+        if not isinstance(train_data, pd.DataFrame):
+            raise AssertionError(
+                f'train_data is required to be a pandas DataFrame, but was instead: {type(train_data)}'
+            )
+
+        if len(set(train_data.columns)) < len(train_data.columns):
+            raise ValueError(
+                "Column names are not unique, please change duplicated column names (in pandas: train_data.rename(columns={'current_name':'new_name'})"
+            )
+        if tuning_data is not None:
+            if not isinstance(tuning_data, pd.DataFrame):
+                raise AssertionError(
+                    f'tuning_data is required to be a pandas DataFrame, but was instead: {type(tuning_data)}'
+                )
+            train_features = [
+                column for column in train_data.columns
+                if column != self.target
+            ]
+            tuning_features = [
+                column for column in tuning_data.columns
+                if column != self.target
+            ]
+            train_features = np.array(train_features)
+            tuning_features = np.array(tuning_features)
+            if np.any(train_features != tuning_features):
+                raise ValueError(
+                    'Column names must match between training and tuning data')
+        return train_data, tuning_data
+
 
 if __name__ == '__main__':
     x, y = sklearn.datasets.load_iris(return_X_y=True)
-    rf = RandomForestClassifier().fit(x, y)
-    print(rf.predict(x))
-    rf = RandomForestHpo(task='multiclass')
+    rf = RandomForestHpo(target=None, task='multiclass')
     rf.fit(x, y)
     print(rf.predict(x))
