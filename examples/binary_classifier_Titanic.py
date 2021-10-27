47eae86e99cb305829ead68450098788b520617a
jianzhnie <jianzhnie@126.com>
2021-10-27 23:03:51 +0800

    add examples

--- examples/binary_classifier_Titanic.py
+++ examples/binary_classifier_Titanic.py
@@ -1,24 +1,24 @@
 import pandas as pd
-import numpy as np
-from supervised.automl import AutoML
 from sklearn.metrics import accuracy_score
+from supervised.automl import AutoML
 
 train = pd.read_csv(
-    "/Users/jianzhengnie/work/AutoTabular/autotabular/datasets/data/Titanic.csv")
+    '/Users/jianzhengnie/work/AutoTabular/autotabular/datasets/data/Titanic.csv'
+)
 print(train.head())
 
 X = train[train.columns[2:]]
-y = train["Survived"]
+y = train['Survived']
 
 validation_strategy = {
-    "validation_type": "split",
-    "train_ratio": 0.8,
-    "shuffle": True,
-    "stratify": True
+    'validation_type': 'split',
+    'train_ratio': 0.8,
+    'shuffle': True,
+    'stratify': True
 }
 
 automl = AutoML(
-    algorithms=["CatBoost", "Xgboost", "LightGBM"],
+    algorithms=['CatBoost', 'Xgboost', 'LightGBM'],
     model_time_limit=30 * 60,
     start_random_models=3,
     hill_climbing_steps=0,
@@ -29,16 +29,17 @@ automl = AutoML(
     train_ensemble=True,
     explain_level=2,
     validation_strategy={
-        "validation_type": "kfold",
-        "k_folds": 2,
-        "shuffle": True,
-        "stratify": True,
+        'validation_type': 'kfold',
+        'k_folds': 2,
+        'shuffle': True,
+        'stratify': True,
     })
 
 automl.fit(X, y)
 
 test = pd.read_csv(
-    "/Users/jianzhengnie/work/AutoTabular/autotabular/datasets/data/Titanic.csv")
+    '/Users/jianzhengnie/work/AutoTabular/autotabular/datasets/data/Titanic.csv'
+)
 predictions = automl.predict(test)
 print(predictions)
 print(f"Accuracy: {accuracy_score(test['Survived'], predictions)*100.0:.2f}%")
